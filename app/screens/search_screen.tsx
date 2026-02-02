6dce226b8a4624b604b8a66a44d9636689a4b27f
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2026-02-02 23:23:28 +0630

    update: search screen

--- app/screens/search_screen.tsx
+++ app/screens/search_screen.tsx
@@ -1,44 +1,87 @@
 // app/_layout.tsx or app/(tabs)/search.tsx
 import { Colors } from "@/constants/theme";
 import { MaterialIcons } from "@expo/vector-icons";
-import { Stack } from "expo-router";
-import { TextInput, View } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { useState } from "react";
+import { Dimensions, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const { width, height } = Dimensions.get('screen');
+
+const SuggestionItem = ({ suggestion_text }: { suggestion_text: string }) => {
+    return (
+        <TouchableOpacity onPress={()=>{}} style={styles.suggestionItem}>
+            <Text>{suggestion_text}</Text>
+        </TouchableOpacity>
+    )
+}
 
 export default function Layout() {
     const color = Colors.light;
-
+    const router = useRouter();
+    const [searchText, setSearchText] = useState<string>('');
     return (
-        <View>
-            <Stack.Screen
-                options={{
-                    title: "Search",
-                    headerTitle: () => (
-                        <View style={{ flex: 1, marginHorizontal: 10 }}>
-                            <TextInput
-                                placeholder="ရှာဖွေရန်..."
-                                placeholderTextColor={color.inActiveIcon}
-                                style={{
-                                    backgroundColor: "#f0eded",
-                                    paddingHorizontal: 10,
-                                    paddingVertical: 8,
-                                    borderRadius: 8,
-                                    fontSize: 16,
-                                    color: color.text,
-                                }}
-                                autoFocus={true}
-                            />
-                        </View>
-                    ),
-                    headerRight: () => (
-                        <MaterialIcons
-                            name="search"
-                            size={28}
-                            color={color.inActiveIcon}
-                            style={{ marginRight: 15 }}
-                        />
-                    ),
-                }}
-            />
-        </View>
+        <SafeAreaView style={{
+            flex: 1
+        }}>
+            <Stack.Screen options={{
+                headerShown: false
+            }} />
+            <View style={{
+                margin: 20,
+                flexDirection: "row",
+                alignItems: 'center',
+                justifyContent: 'flex-start',
+                gap: 10
+            }}>
+                <MaterialIcons name="arrow-back" size={28}
+                    onPress={() => router.back()}
+                    style={{
+                        width: 20,
+                    }}
+                />
+                <TextInput
+                    placeholder="ရှာဖွေရန်..."
+                    placeholderTextColor={color.inActiveIcon}
+                    style={{
+                        backgroundColor: "#fffefe",
+                        paddingHorizontal: 20,
+                        paddingVertical: 8,
+                        borderRadius: 8,
+                        fontSize: 16,
+                        width: width - 100,
+                        color: color.text,
+                    }}
+                    value={searchText}
+                    autoFocus={true}
+                    onChangeText={(text) => setSearchText(text)}
+                />
+
+                <MaterialIcons
+                    name="search"
+                    size={28}
+                    color={color.inActiveIcon}
+                    style={{ width: 20 }}
+                />
+            </View>
+            {/* suggestion views */}
+            <View style={styles.suggestionViewContainer}>
+                <SuggestionItem suggestion_text="Environment issues" />
+                <SuggestionItem suggestion_text="SOmething searched" />
+            </View>
+            {/* searched contents views */}
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    suggestionViewContainer: {
+        flex: 1,
+        backgroundColor: Colors.light.background
+    },
+    suggestionItem: {
+        padding: 20,
+        color: 'black',
+        fontWeight: 'bold'
+    }
+})
